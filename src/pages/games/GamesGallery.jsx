8ec752d72bf197ebc87f1b27e5ed834916808af1
Pablo de Andres <pablodeandresolano@gmail.com>
2022-08-23 21:32:08 +0200

    Add pagination

--- src/pages/games/GamesGallery.jsx
+++ src/pages/games/GamesGallery.jsx
@@ -5,12 +5,19 @@ import { Link } from "react-router-dom";
 
 function GamesGallery() {
   const [games, setGames] = useState([]);
-  
-  
+  const [page, setPage] = useState(1);
+  const [hasPrevious, setHasPrevious] = useState("");
+  const [hasNext, setHasNext] = useState("");
+
   const getGames = async () => {
     try {
-      const response = await axios( "https://api.rawg.io/api/games?key=848748eade3647ecbf3ac299d1c7b50c&page_size=10" );
+      const response = await axios(
+        `https://api.rawg.io/api/games?key=848748eade3647ecbf3ac299d1c7b50c&page_size=9&page=${page}`
+      );
+
       setGames(response.data.results);
+      setHasNext(response.data.next);
+      setHasPrevious(response.data.previous);
     } catch (error) {
       setGames([]);
     }
@@ -18,21 +25,47 @@ function GamesGallery() {
 
   useEffect(() => {
     getGames();
-  }, []);
+  }, [page]);
+
+  const handleNextPage = () => {
+    setPage((previousValue) => previousValue + 1);
+  };
+
+  const handlePreviousPage = () => {
+    setPage((previousValue) => previousValue - 1);
+  };
 
   return (
-    <div>
-      {games.map((eachGame) => {
-        return (
-          //TODO Cambiar key por {eachGame._id}
-          <Link key={eachGame.id} to={`/games/${eachGame.id}`}>
-            <div style={{ border: "1px solid black" }}>
-              <h2>{eachGame.name}</h2>
-              <img src={eachGame.background_image} height="200px" alt="" />
-            </div>
-          </Link>
-        );
-      })}
+    <div className="wrapper">
+      <div className="grid">
+        {games.map((eachGame) => {
+          return (
+            <Link
+              className="grid-element"
+              key={eachGame.id}
+              to={`/games/${eachGame.id}`}
+            >
+              <div>
+                <h2>{eachGame.name}</h2>
+                <img src={eachGame.background_image} height="200px" alt="" />
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+
+      <div className="pagination">
+        <button
+          className="button"
+          disabled={!hasPrevious}
+          onClick={handlePreviousPage}
+        >
+          Previous
+        </button>
+        <button className="button" disabled={!hasNext} onClick={handleNextPage}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
