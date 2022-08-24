bdc48ce27698e6d34474ae650a9b8a265a097a8f
Pablo de Andres <pablodeandresolano@gmail.com>
2022-08-24 11:41:10 +0200

    Add user services

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -3,4 +3,27 @@ import service from "./config.services";
 const findUserService = () => {
   return service.get(`/profile/my-profile`);
 };
-export { findUserService };
+
+const editUserService = (userId) => {
+  return service.patch(`/profile/${userId}/edit`);
+};
+
+const findOneUserService = (userId) => {
+  return service.get(`/profile/${userId}`);
+};
+
+const blockUserAccount = (userId) => {
+  return service.patch(`/close-account`);
+};
+
+const favouriteGameService = (gameId) => {
+  return service.patch(`/favourites/${gameId}`);
+};
+
+export {
+  findUserService,
+  editUserService,
+  findOneUserService,
+  blockUserAccount,
+  favouriteGameService,
+};
